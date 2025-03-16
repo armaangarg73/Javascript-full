274c4962dd4debc11fd703c755e0dee3d0fc6781
armaangarg73 <149618732+armaangarg73@users.noreply.github.com>
2025-03-16 11:16:30 +0000

    conversion

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,31 @@
+// let score = "98abc";
+// let score = true;
+// let score = null;
+// let score = undefined;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let value = Number(score);
+// console.log(typeof value);
+// console.log(value);
+
+/*
+"98" -> number
+"98abc" -> NAN
+true -> 1 , false -> 0
+*/
+
+// let isLoggedIn = "Armaan" ;
+// let booleanLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanLoggedIn) ;
+
+/*
+1 ->true , 0->false 
+"Armaan"-> true
+*/
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(someNumber);
+console.log(typeof stringNumber);
