d98588f458f6254e13da6bfb6dccb2dc58f4dc4e
armaangarg73 <149618732+armaangarg73@users.noreply.github.com>
2025-03-16 10:39:06 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "Armaan" ;
+let age = 19 ;
+let isloggedIn = false ;
+let state = null ;
+let account ;
+
+/*
+
+Primitive Datatypes:-
+
+number -> 2 to the power 53
+bigInt -> facebook , reddit , stocks etc
+string -> ""
+boolean -> true/false 
+null
+undefined ->
+symbol -> uniqueness
+*/
+
+// Object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof isloggedIn);
