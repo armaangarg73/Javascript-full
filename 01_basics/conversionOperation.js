b574a531b6c672767be059484a3275790a99585b
armaangarg73 <149618732+armaangarg73@users.noreply.github.com>
2025-03-18 05:02:21 +0000

    conversion

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -27,5 +27,39 @@ true -> 1 , false -> 0
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log(someNumber);
-console.log(typeof stringNumber);
+// console.log(someNumber);
+// console.log(typeof stringNumber);
+
+// **********Operations**********
+
+let value = 3 ;
+let negValue = -value ;
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello" ;
+let str2 = " Armaan" ;
+
+let str3 = str1 + str2 ;
+// console.log(str3);
+
+// console.log("1" + 2) ;
+// console.log(1 + "2") ;
+// console.log("1" + 2 + 2) ; // string -> first hai to sbhi ko string dekhenge
+// console.log(1 + 2 + "2") ; // string last hai to last se string conversion hoga aur usse pehhle vale apka number hoga
+
+// Bad practices -> code becoming complex
+// console.log(+true); //1 
+// console.log(+"") ; // 0
+ 
+let gameCounter = 200 ;
+// gameCounter++; // Postfix
+++gameCounter ;// Prefix
+console.log(gameCounter) ;
